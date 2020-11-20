5006d8c00b2eaf43936d9a2afd1d32276658d797
cmdanicich <claire.danicich@umontana.edu>
2020-11-19 21:58:00 -0700

    deleted keys

--- My_API_Keys.py
+++
@@ -1,4 +0,0 @@
-api_key = '7ZcY7VLITotQof66HWxpLEcLI'
-api_key_secret = 'Mb9m42eRuGbS3vs6QDMisjfBI70Xshd2Tvu1KhMyO21wsVRQVd'
-access_token = '1153364828615921664-kfWwYyTFsJMWGF0VDDWudLQI3e3XpZ'
-access_token_secret = '6SMIexrTqfkJjXApwaPYw5EGiIYnzWkgEhsFh15lHaWgU'
